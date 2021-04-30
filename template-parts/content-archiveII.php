ec0258a997ae3068333ff4497007a34720d6df2e
mathia-gowb <Tobvins@gmail.com>
2021-04-30 14:52:07 +0200

    done with the custom-preview page

---
+++ template-parts/content-archiveII.php
@@ -0,0 +1,9 @@
+<article class="popular">
+    <figure class="popular-program-article">
+        <img src="<?=the_post_thumbnail_url('medium')?>" alt="blog image" alt="" srcset="">
+        <figcaption class="popular-article-name">
+        <br>
+        <p><a href="#" class="white-link"><?=the_title()?></a></p>
+        </figcaption>
+    </figure>
+</article>
